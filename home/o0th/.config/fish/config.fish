9b870408d868c9c17c7857de750e52faf61696b3
o0th <o0th@pm.me>
2024-11-26 15:27:36 +0100

    feat: cargo bin

--- home/o0th/.config/fish/config.fish
+++ home/o0th/.config/fish/config.fish
@@ -1,10 +1,20 @@
 if status is-interactive
+
+	# Start tmux on startup
+	if not set -q TMUX
+		exec tmux
+	end
+
 	# Remove greeting message
 	set -U fish_greeting
 
 	# Start starship
 	starship init fish | source
 
+	# Path
+	fish_add_path ~/.cargo/bin
+
+	# Aliases
 	alias v='nvim'
 	alias dot='/usr/bin/git --git-dir=/home/o0th/.dotfiles --work-tree=/'
 end
