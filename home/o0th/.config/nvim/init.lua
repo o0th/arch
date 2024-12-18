01f5e7480047bea708254a9a41bc7f7346eb90de
o0th <o0th@pm.me>
2024-12-18 21:12:12 +0100

    feat(nvim): autoindent, yank on "

--- home/o0th/.config/nvim/init.lua
+++ home/o0th/.config/nvim/init.lua
@@ -3,10 +3,10 @@ require("config.lazy")
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.swapfile = false
 vim.opt.undofile = true
-vim.opt.smartindent = true
 vim.opt.signcolumn = 'yes'
 vim.opt.scrolloff = 10
-
+vim.opt.autoindent = true
 vim.wo.number = true
 
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+vim.keymap.set("v", "y", '"+y', { noremap = true })
