1c7e0c3d52f4e8c7ae3a92dad1930d8f49a82216
o0th <o0th@pm.me>
2024-12-20 15:08:13 +0100

    feat(nvim): blink

--- home/o0th/.config/nvim/lua/plugins/blink.lua
+++ home/o0th/.config/nvim/lua/plugins/blink.lua
@@ -2,7 +2,8 @@ return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = {
-    { 'rafamadriz/friendly-snippets'},
+    { 'rafamadriz/friendly-snippets' },
+    { 'giuxtaposition/blink-cmp-copilot' },
   },
 
   -- use a release tag to download pre-built binaries
@@ -29,21 +30,72 @@ return {
       use_nvim_cmp_as_default = true,
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font
       -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+      nerd_font_variant = 'mono',
+
+      kind_icons = {
+        Copilot = "",
+        Text = '󰉿',
+        Method = '󰊕',
+        Function = '󰊕',
+        Constructor = '󰒓',
+
+        Field = '󰜢',
+        Variable = '󰆦',
+        Property = '󰖷',
+
+        Class = '󱡠',
+        Interface = '󱡠',
+        Struct = '󱡠',
+        Module = '󰅩',
+
+        Unit = '󰪚',
+        Value = '󰦨',
+        Enum = '󰦨',
+        EnumMember = '󰦨',
+
+        Keyword = '󰻾',
+        Constant = '󰏿',
+
+        Snippet = '󱄽',
+        Color = '󰏘',
+        File = '󰈔',
+        Reference = '󰬲',
+        Folder = '󰉋',
+        Event = '󱐋',
+        Operator = '󰪚',
+        TypeParameter = '󰬛',
+      },
     },
 
     -- default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, via `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        copilot = {
+          name = 'copilot',
+          module = 'blink-cmp-copilot',
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        }
+      },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
       -- optionally disable cmdline completions
       -- cmdline = {},
     },
 
     -- experimental signature help support
-    signature = { enabled = true }
+    signature = { enabled = true },
   },
   -- allows extending the providers array elsewhere in your config
   -- without having to redefine it
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
